92df5eb929cb9066febeeef86b1cf2dc2f17f91c
dazfuller <daz.fuller@gmail.com>
2017-05-19 12:19:40 +0100

    Modified how unit tests are executed to avoid stderr issues in VSTS

--- vsts_build.ps1
+++ vsts_build.ps1
@@ -1,5 +1,13 @@
+Param (
+    [string] $OutputLocation
+)
+
 python -m venv .\venv\build
 .\venv\build\Scripts\Activate.ps1
 pip install -r requirements.txt
-coverage run -m unittest discover
-coverage xml
+
+Write-Output "> Executing unit tests"
+& coverage run -m unittest discover 2>execute_stderr.txt
+
+Write-Output "> Generating coverage report"
+& coverage xml -o "$($OutputLocation)\coverage.xml" 2>coverage_stderr.txt
