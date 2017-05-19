aeb2fee28e6894e820faf06eacc0298f22ab98f7
dazfuller <daz.fuller@gmail.com>
2017-05-19 12:32:00 +0100

    Added documentation to build script

--- vsts_build.ps1
+++ vsts_build.ps1
@@ -1,3 +1,21 @@
+<#
+.SYNOPSIS
+    VSTS build script
+.DESCRIPTION
+    A custom script to create and activate the virtual environment, execute unit tests
+    and produce coverage information
+.EXAMPLE
+    PS C:\> .\vsts_build.ps1 -OutputLocation .
+    Executes the script and generates the coverage XML output in the current directory
+.INPUTS
+    OutputLocation - The location to write the coverage XML file
+.OUTPUTS
+    None
+.NOTES
+    Currently this re-directs the stderr output of the unittests and coverage commands, 
+    this is because the TestTextRunning by default outputs to stderr which can cause
+    a build failure in VSTS
+#>
 Param (
     [string] $OutputLocation
 )
